b7f469e80631edecadaf52cecd3532ed9bd96da7
Ryo Nakamura <r7kamura@gmail.com>
2015-08-02 01:42:51 +0900

    Add JsonRequestEncoder middleware

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 module.exports = {
   Fetcher: require('./src/fetcher'),
+  JsonRequestEncoder: require('./src/json-request-encoder'),
   Request: require('./src/request'),
   RequestLogger: require('./src/request-logger'),
   ResponseLogger: require('./src/response-logger')
